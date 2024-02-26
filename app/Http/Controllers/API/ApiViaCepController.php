2f07001e50ab72c84947a2692bd5258ed8efcecd
ThiagoZamprognioBahia <zcranigan11gmail.com>
2024-02-26 01:58:29 -0300

    first commit

---
+++ app/Http/Controllers/API/ApiViaCepController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
+
+class ApiViaCepController extends Controller
+{
+    public function show($cep)
+    {
+        $validator = $this->validateCep($cep);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->all()], 400);
+        }
+    
+        try {
+            $apiUrl = Config::get('services.viacep.url');
+            $response = Http::get($apiUrl . $cep . '/json');
+            $data = $response->json();
+            if (isset($data['erro']) && $data['erro'] == true) {
+                return response()->json(['message' => 'Esse CEP não existe.'], 404);
+            }
+            return $data;
+        } catch (RequestException $e) {
+            return response()->json(['message' => 'Erro ao buscar dados do CEP.'],  $e->getCode() ?: 500);
+        }
+    }
+    
+    protected function validateCep($cep)
+    {
+        return Validator::make(['cep' => $cep], [
+            'cep' => ['regex:/^\d{8}$/'],
+        ], [
+            'cep.regex' => 'O formato do CEP é inválido. Deve conter apenas 8 dígitos numéricos.',
+        ]);
+    }
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('throttle:60,1')->namespace('API')->group(function() {
+
+    Route::get('/consultar-api/{cep}', ApiViaCepController::class . '@show');
+
+});
